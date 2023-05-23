382eba4a99999bf960c72eff87171cdfbabfc383
Zeioth <zeioth@hotmail.com>
2023-05-23 19:01:57 +0200

    Added: rnvimr final (for now)

--- lua/plugins/1-base-behaviors.lua
+++ lua/plugins/1-base-behaviors.lua
@@ -1,7 +1,6 @@
 -- Core behaviors
 -- Things that add new behaviors.
 
-
 --    Sections:
 --       -> ranger file browser    [ranger]
 --       -> project.nvim           [project search + auto cd]
@@ -16,64 +15,108 @@
 --       -> neotree file browser   [neotree]
 --       -> nvim-ufo               [folding mod]
 
-
-
 -- import custom icons
 local get_icon = require("base.utils").get_icon
-
 
 -- configures plugins
 return {
   -- [ranger] file browser (fork with mouse scroll support)
   -- https://github.com/Zeioth/ranger.vim
   {
+    -- This one is a backup ranger in case rnvimr breaks for some reason.
     "zeioth/ranger.vim",
-    dependencies = {"rbgrouleff/bclose.vim"},
-     cmd = { "Ranger" },
-     init = function()
-       vim.g.ranger_terminal = 'foot'
-       vim.g.ranger_command_override = 'LC_ALL=es_ES.UTF8 TERMCMD="foot -a \"scratchpad\"" ranger'
-       vim.g.ranger_map_keys = 0
-     end
+    dependencies = { "rbgrouleff/bclose.vim" },
+    cmd = { "Ranger" },
+    init = function() -- For this plugin has to be init
+      vim.g.ranger_terminal = "foot"
+      vim.g.ranger_command_override = 'LC_ALL=es_ES.UTF8 TERMCMD="foot -a "scratchpad"" ranger'
+      vim.g.ranger_map_keys = 0
+    end,
   },
-
-
-
-
-  -- TODO: WIP: Currently broken
-  --       - Buffers open on float beause pynvim issuer supposedly.
   {
     "kevinhwang91/rnvimr",
-     cmd = { "RnvimrToggle" },
-     init = function ()
-       vim.g.rnvimr_enable_picker = 1
-       vim.g.rnvimr_ranger_cmd = { 'ranger' } -- use ranger_custom to enable term
+    cmd = { "RnvimrToggle" },
+    init = function()
+      -- vim.g.rnvimr_vanilla = 1 → Often solves many issues
+      vim.g.rnvimr_enable_picker = 1 -- if 1, will replace the current buffer
+      vim.g.rnvimr_ranger_cmd = { "ranger" }
 
-     end
+      -- Known bug: We cannot set a custom terminal for rnvimr.
+      --            (now even thourh a wrapper exe)
+      --            Doing so will provoke rnvimr RPC to not close correctly.
+      --            If you need that use ranger.vim
+      --
+      -- Check: https://github.com/kevinhwang91/rnvimr/issues/149
+      -- vim.g.rnvimr_ranger_cmd = { "ranger-custom" } -- Currently broken
+    end,
+    config = function() -- FIX: Adds mouse support to rnvimr
+      -- TODO: You can delete this after the author merges it:
+      -- https://github.com/kevinhwang91/rnvimr/issues/58
+      local augroup = vim.api.nvim_create_augroup
+      local autocmd = vim.api.nvim_create_autocmd
+
+      local rnvimr_mouse_group = augroup("RnvimrMouse", { clear = true })
+
+      -- Enables mouse support for rnvimr
+      local function set_mouse_with_rnvimr()
+        local n_mouse = vim.o.mouse
+
+        -- Disable nvim mouse support while we are on the rnvimr buffer
+        if string.match(n_mouse, "[a|h|n]") then
+          autocmd({ "TermEnter", "WinEnter <buffer>" }, {
+            desc = "Disable nvim mouse support while we are on the rnvimr buffer",
+            group = rnvimr_mouse_group,
+            callback = function() vim.api.nvim_set_option("mouse", "") end,
+          })
+        end
+
+        -- Extra mouse fix for tmux
+        -- If tmux mouse mode is enabled
+        local output = vim.fn.system 'tmux display -p "#{mouse}"'
+        if output:sub(1, 1) == "1" then
+          -- Disable tmux mouse while using rnvimr
+          autocmd({ "TermEnter", "WinEnter <buffer>" }, {
+            desc = "Disable tmux mouse while using rnvimr",
+            group = rnvimr_mouse_group,
+            callback = function() vim.fn.system "tmux set mouse off" end,
+          })
+
+          -- Enable tmux mouse when mouse leaves rnvimr
+          autocmd({ "WinLeave" }, {
+            desc = "Enable tmux mouse when mouse leaves rnvimr",
+            group = rnvimr_mouse_group,
+            callback = function() vim.fn.system "tmux set mouse off" end,
+          })
+        end
+      end
+
+      -- Entry point
+      autocmd({ "FileType rnvimr" }, {
+        desc = "If we are on the rnvimr buffer, execute the callback",
+        group = rnvimr_mouse_group,
+        callback = function()
+          if vim.bo.filetype == "rnvimr" then set_mouse_with_rnvimr() end
+        end,
+      })
+    end,
   },
-
-
 
   -- project.nvim [project search + auto cd]
   -- https://github.com/ahmedkhalf/project.nvim
   {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
-    init = function ()
+    opts = {
       -- How to find root directory
-      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" }
-      silent_chdir = false
-      manual_mode = false
-    end,
-    opts = { ignore_lsp = { "lua_ls" } },
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+      silent_chdir = false,
+      manual_mode = false,
+      --ignore_lsp = { "lua_ls" },
+    },
     config = function(_, opts) require("project_nvim").setup(opts) end,
   },
   -- Telescope integration (:Telescope projects)
   { "nvim-telescope/telescope.nvim", opts = function() require("telescope").load_extension "projects" end },
-
-
-
-
 
   -- trim.nvim [auto trim spaces]
   -- https://github.com/cappyzawa/trim.nvim
@@ -90,37 +133,39 @@ return {
     },
   },
 
-
-
-
   -- stay-centered.nvim [cursor centered]
   -- https://github.com/arnamak/stay-centered.nvim
   {
     "arnamak/stay-centered.nvim",
-     lazy=false,
-     opts = {
-       skip_filetypes = {
-         "alpha",
-         "ranger",
-         "rnvimr",
-         "neotree",
-         "neotree-popup",
-         "spectre_panel",
-         "aerial",
-         "aerial-nav" }
-     }
+    lazy = false,
+    opts = {
+      skip_filetypes = {
+        "ranger",
+        "rnvimr",
+        "neotree",
+        "NvimTree",
+        "neo-tree",
+        "neotree-popup",
+        "spectre_panel",
+        "help",
+        "startify",
+        "aerial",
+        "aerial-nav",
+        "alpha",
+        "dashboard",
+        "lazy",
+        "neogitstatus",
+        "Trouble",
+      },
+    },
   },
-
-
-
 
   -- stickybuf.nvim [lock special buffers]
   -- https://github.com/arnamak/stay-centered.nvim
   -- By default it support neovim/aerial and others.
   {
-    "stevearc/stickybuf.nvim"
+    "stevearc/stickybuf.nvim",
   },
-
 
   -- telescope-undo.nvim
   -- https://github.com/debugloop/telescope-undo.nvim
@@ -138,15 +183,12 @@ return {
   -- Telescope integration (:Telescope projects)
   { "nvim-telescope/telescope.nvim", opts = function() require("telescope").load_extension "undo" end },
 
-
   -- easier window selection  [windows]
   -- https://github.com/s1n7ax/nvim-window-picker
   {
-    "s1n7ax/nvim-window-picker", opts = { use_winbar = "smart" }
+    "s1n7ax/nvim-window-picker",
+    opts = { use_winbar = "smart" },
   },
-
-
-
 
   -- Improved [esc]
   -- https://github.com/max397574/better-escape.nvim
@@ -156,11 +198,8 @@ return {
     opts = {
       mapping = {},
       timeout = 300,
-    }
+    },
   },
-
-
-
 
   -- Toggle floating terminal on <F7> [term]
   -- https://github.com/akinsho/toggleterm.nvim
@@ -178,9 +217,6 @@ return {
       },
     },
   },
-
-
-
 
   -- Session management [session]
   -- TODO: Replace both for procession or similar.
@@ -201,9 +237,6 @@ return {
     },
   },
 
-
-
-
   -- spectre.nvim [search and replace in project]
   -- https://github.com/nvim-pack/nvim-spectre
   -- It doesn't have ctrl-z so please always commit before using it.
@@ -211,99 +244,95 @@ return {
     "nvim-pack/nvim-spectre",
     cmd = "Spectre",
     opts = {
-    find_engine = {
+      find_engine = {
         default = {
-            find = {
-                --pick one of item in find_engine [ag, rg ]
-                cmd = "ag",
-                options = {"ignore-case"}
-            },
-            replace={
-                -- If you install oxi with cargo you can use it instead.
-                cmd = "sed"
-            }
+          find = {
+            --pick one of item in find_engine [ag, rg ]
+            cmd = "ag",
+            options = { "ignore-case" },
+          },
+          replace = {
+            -- If you install oxi with cargo you can use it instead.
+            cmd = "sed",
+          },
         },
       },
-      mapping={ -- The mappings only appear while spectre is active
-          ['toggle_line'] = {
-              map = "dd",
-              cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
-              desc = "toggle current item"
-          },
-          ['enter_file'] = {
-              map = "<cr>",
-              cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
-              desc = "goto current file"
-          },
-          ['send_to_qf'] = {
-              map = "<leader>q",
-              cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
-              desc = "send all item to quickfix"
-          },
-          ['replace_cmd'] = {
-              map = "<leader>c",
-              cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
-              desc = "input replace vim command"
-          },
-          ['show_option_menu'] = {
-              map = "<leader>o",
-              cmd = "<cmd>lua require('spectre').show_options()<CR>",
-              desc = "show option"
-          },
-          ['run_current_replace'] = {
-            map = "<leader>rc",
-            cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
-            desc = "replace current line"
-          },
-          ['run_replace'] = {
-              map = "<leader>T",
-              cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
-              desc = "replace all"
-          },
-          ['change_view_mode'] = {
-              map = "<leader>v",
-              cmd = "<cmd>lua require('spectre').change_view()<CR>",
-              desc = "change result view mode"
-          },
-          ['change_replace_sed'] = {
-            map = "trs",
-            cmd = "<cmd>lua require('spectre').change_engine_replace('sed')<CR>",
-            desc = "use sed to replace"
-          },
-          ['change_replace_oxi'] = {
-            map = "tro",
-            cmd = "<cmd>lua require('spectre').change_engine_replace('oxi')<CR>",
-            desc = "use oxi to replace"
-          },
-          ['toggle_live_update']={
-            map = "tu",
-            cmd = "<cmd>lua require('spectre').toggle_live_update()<CR>",
-            desc = "update change when vim write file."
-          },
-          ['toggle_ignore_case'] = {
-            map = "ti",
-            cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
-            desc = "toggle ignore case"
-          },
-          ['toggle_ignore_hidden'] = {
-            map = "th",
-            cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
-            desc = "toggle search hidden"
-          },
-          ['resume_last_search'] = {
-            map = "<leader>l",
-            cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
-            desc = "resume last search before close"
-          },
-          -- you can put your mapping here it only use normal mode
+      mapping = {
+        -- The mappings only appear while spectre is active
+        ["toggle_line"] = {
+          map = "dd",
+          cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
+          desc = "toggle current item",
         },
-
+        ["enter_file"] = {
+          map = "<cr>",
+          cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+          desc = "goto current file",
+        },
+        ["send_to_qf"] = {
+          map = "<leader>q",
+          cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+          desc = "send all item to quickfix",
+        },
+        ["replace_cmd"] = {
+          map = "<leader>c",
+          cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+          desc = "input replace vim command",
+        },
+        ["show_option_menu"] = {
+          map = "<leader>o",
+          cmd = "<cmd>lua require('spectre').show_options()<CR>",
+          desc = "show option",
+        },
+        ["run_current_replace"] = {
+          map = "<leader>rc",
+          cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+          desc = "replace current line",
+        },
+        ["run_replace"] = {
+          map = "<leader>T",
+          cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+          desc = "replace all",
+        },
+        ["change_view_mode"] = {
+          map = "<leader>v",
+          cmd = "<cmd>lua require('spectre').change_view()<CR>",
+          desc = "change result view mode",
+        },
+        ["change_replace_sed"] = {
+          map = "trs",
+          cmd = "<cmd>lua require('spectre').change_engine_replace('sed')<CR>",
+          desc = "use sed to replace",
+        },
+        ["change_replace_oxi"] = {
+          map = "tro",
+          cmd = "<cmd>lua require('spectre').change_engine_replace('oxi')<CR>",
+          desc = "use oxi to replace",
+        },
+        ["toggle_live_update"] = {
+          map = "tu",
+          cmd = "<cmd>lua require('spectre').toggle_live_update()<CR>",
+          desc = "update change when vim write file.",
+        },
+        ["toggle_ignore_case"] = {
+          map = "ti",
+          cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
+          desc = "toggle ignore case",
+        },
+        ["toggle_ignore_hidden"] = {
+          map = "th",
+          cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
+          desc = "toggle search hidden",
+        },
+        ["resume_last_search"] = {
+          map = "<leader>l",
+          cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
+          desc = "resume last search before close",
+        },
+        -- you can put your mapping here it only use normal mode
+      },
     },
-
   },
-
-
-
 
   --neotree
   -- https://github.com/nvim-neo-tree/neo-tree.nvim
@@ -320,12 +349,11 @@ return {
         winbar = true,
         content_layout = "center",
         sources = {
-        { source = "filesystem", display_name = get_icon "FolderClosed" .. " File" },
-        { source = "buffers", display_name = get_icon "DefaultFile" .. " Bufs" },
-        { source = "git_status", display_name = get_icon "Git" .. " Git" },
-        { source = "diagnostics", display_name = get_icon "Diagnostic" .. " Diagnostic" },
-      },
-
+          { source = "filesystem",  display_name = get_icon "FolderClosed" .. " File" },
+          { source = "buffers",     display_name = get_icon "DefaultFile" .. " Bufs" },
+          { source = "git_status",  display_name = get_icon "Git" .. " Git" },
+          { source = "diagnostics", display_name = get_icon "Diagnostic" .. " Diagnostic" },
+        },
       },
       default_component_configs = {
         indent = { padding = 0, indent_size = 1 },
@@ -365,7 +393,7 @@ return {
           if node.type == "directory" or node:has_children() then
             if not node:is_expanded() then -- if unexpanded, expand
               state.commands.toggle_node(state)
-            else -- if expanded and has children, seleect the next child
+            else                           -- if expanded and has children, seleect the next child
               require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
             end
           else -- if not a directory just open it
@@ -393,9 +421,9 @@ return {
           for i, result in pairs(results) do
             if result.val and result.val ~= "" then
               vim.list_extend(messages, {
-                { ("%s."):format(i), "Identifier" },
+                { ("%s."):format(i),           "Identifier" },
                 { (" %s: "):format(result.msg) },
-                { result.val, "String" },
+                { result.val,                  "String" },
                 { "\n" },
               })
             end
@@ -407,12 +435,10 @@ return {
             vim.fn.setreg("+", result.val)
           end
         end,
-        run_command = function(state)
-          vim.api.nvim_input(":")
-        end,
+        run_command = function(state) vim.api.nvim_input ":" end,
         diff_files = function(state)
           local node = state.tree:get_node()
-          local log = require("neo-tree.log")
+          local log = require "neo-tree.log"
           state.clipboard = state.clipboard or {}
           if diff_Node and diff_Node ~= tostring(node.id) then
             local current_Diff = node.id
@@ -445,16 +471,16 @@ return {
           ["<space>"] = false, -- disable space until we figure out which-key disabling
           ["[b"] = "prev_source",
           ["]b"] = "next_source",
-          ['e'] = function() vim.api.nvim_exec('Neotree focus filesystem left', true) end,
-          ['b'] = function() vim.api.nvim_exec('Neotree focus buffers left', true) end,
-          ['g'] = function() vim.api.nvim_exec('Neotree focus git_status left', true) end,
+          ["e"] = function() vim.api.nvim_exec("Neotree focus filesystem left", true) end,
+          ["b"] = function() vim.api.nvim_exec("Neotree focus buffers left", true) end,
+          ["g"] = function() vim.api.nvim_exec("Neotree focus git_status left", true) end,
           o = "open",
           O = "system_open",
           h = "parent_or_close",
           l = "child_or_open",
           Y = "copy_selector",
           ["s"] = "run_command",
-          ['D'] = "diff_files", -- This replaces filter directories
+          ["D"] = "diff_files", -- This replaces filter directories
         },
       },
       filesystem = {
@@ -509,14 +535,13 @@ return {
         end
 
         return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or function(bufnr)
-            return require("ufo")
-              .getFolds(bufnr, "lsp")
-              :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
-              :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
-          end
+            or function(bufnr)
+              return require("ufo")
+                  .getFolds(bufnr, "lsp")
+                  :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+                  :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+            end
       end,
     },
   },
-
 }
