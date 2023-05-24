beafa8eca9bcac84804da3342d31a00737abd104
Zeioth <zeioth@hotmail.com>
2023-05-24 19:07:14 +0200

    Added: Theme colors exposed to the config file.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -35,8 +35,90 @@ return {
 
       -- Colors can be overrided
       on_colors = function(colors)
-        colors.hint = colors.orange
-        colors.error = "#ff0000"
+        -- colors.bg_dark = "#16161e" -- Other backgrounds
+        -- colors.bg = "#1a1b26" -- Main background color
+        -- colors.bg_highlight = "#2f334d" -- Active line background color
+        -- colors.terminal_black = "#444a73" -- No idea really
+        -- colors.fg = "#c8d3f5" -- UI regular text color (including greeter)
+        -- colors.fg_dark = "#828bb8" -- Parentesis and status echo text
+        -- colors.fg_gutter = "#3b4261" -- Versical line separators
+        -- colors.dark3 = "#545c7e"
+        -- colors.comment = "#7a88cf"
+        -- colors.dark5 = "#737aa2"
+        -- colors.blue0 = "#3e68d7"
+        -- colors.blue = "#82aaff"
+        -- colors.cyan = "#86e1fc"
+        -- colors.blue1 = "#65bcff"
+        -- colors.blue2 = "#0db9d7"
+        -- colors.blue5 = "#89ddff"
+        -- colors.blue6 = "#b4f9f8"
+        -- colors.blue7 = "#394b70"
+        -- colors.purple = "#fca7ea"
+        -- colors.magenta2 = "#ff007c"
+        -- colors.magenta = "#c099ff"
+        -- colors.orange = "#ff966c"
+        -- colors.yellow = "#ffc777"
+        -- colors.green = "#c3e88d"
+        -- colors.green1 = "#4fd6be"
+        -- colors.green2 = "#41a6b5"
+        -- colors.teal = "#4fd6be"
+        -- colors.red = "#ff757f"
+        -- colors.red1 = "#c53b53"
+
+        -- colorcolumn (80 col separator)
+        --vim.cmd "hi colorcolumn guibg='#000000'"
+
+        -- -- GIT + GITSIGN
+        -- colors.git =
+        --   { change = "#6183bb", add = "#449dab", delete = "#914c54" }
+        -- colors.gitSigns = {
+        --   add = "#266d6a",
+        --   change = "#536c9e",
+        --   delete = "#b2555b",
+        -- }
+        --
+        -- -- DIFF
+        -- colors.diff = {
+        --   add = util.darken(colors.green2, 0.15),
+        --   delete = util.darken(colors.red1, 0.15),
+        --   change = util.darken(colors.blue7, 0.15),
+        --   text = colors.blue7,
+        -- }
+        --
+        -- -- POPUP AND STPOPUP AND STATUS BG
+        -- colors.bg_popup = colors.bg_dark
+        -- colors.bg_statusline = colors.bg_dark
+        --
+        -- -- SEARCH AND VISUAL SELECTION FROM A LIST BG
+        -- -- colors.bg_visual = util.darken(colors.blue0, 0.4) -- bg when you select stuff from a list of elements ()
+        -- colors.bg_search = colors.blue0
+        --
+        -- -- SIDEBAR
+        -- colors.fg_sidebar = colors.fg_dark -- Sidebar and status bar text color
+        -- colors.bg_sidebar = config.options.styles.sidebars == "transparent"
+        --     and colors.none
+        --   or config.options.styles.sidebars == "dark" and colors.bg_dark
+        --   or colors.bg
+        --
+        -- -- FLOAT
+        -- colors.fg_float = colors.fg
+        -- colors.bg_float = config.options.styles.floats == "transparent"
+        --     and colors.none
+        --   or config.options.styles.floats == "dark" and colors.bg_dark
+        --   or colors.bg
+
+        -- ERRORS
+        -- colors.error = colors.red1
+        -- colors.warning = colors.yellow
+        -- colors.info = colors.blue2
+        -- colors.hint = colors.teal
+
+        -- DELTA
+        -- Colors when delta is enablet
+        -- colors.delta = {
+        --   add = util.darken(colors.green2, 0.45),
+        --   delete = util.darken(colors.red1, 0.45),
+        -- }
       end,
     },
   },
@@ -106,7 +188,11 @@ return {
         dashboard.button("n", "📄 New     ", "<cmd>ene<CR>"),
         dashboard.button("e", "🌺 Recent  ", "<cmd>Telescope oldfiles<CR>"),
         dashboard.button("r", "🐍 Ranger  ", "<cmd>RnvimrToggle<CR>"),
-        dashboard.button("s", "🔎 Sessions", "<cmd>SessionManager! load_session<CR>"),
+        dashboard.button(
+          "s",
+          "🔎 Sessions",
+          "<cmd>SessionManager! load_session<CR>"
+        ),
         dashboard.button("p", "💼 Projects", "<cmd>Telescope projects<CR>"),
         dashboard.button("", ""),
         dashboard.button("q", "   Quit", ":exit<CR>"),
@@ -114,8 +200,10 @@ return {
       }
 
       ---- Vertical margins
-      dashboard.config.layout[1].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.10) } -- Above header
-      dashboard.config.layout[3].val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.10) } -- Above buttons
+      dashboard.config.layout[1].val =
+        vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.10) } -- Above header
+      dashboard.config.layout[3].val =
+        vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.10) } -- Above buttons
 
       -- Disablel autocmd and return
       dashboard.config.opts.noautocmd = true
@@ -150,8 +238,14 @@ return {
   --  https://github.com/rcarriga/nvim-notify
   {
     "rcarriga/nvim-notify",
-    init = function() require("base.utils").load_plugin_with_func("nvim-notify", vim, "notify") end,
-    opts = { on_open = function(win) vim.api.nvim_win_set_config(win, { zindex = 1000 }) end },
+    init = function()
+      require("base.utils").load_plugin_with_func("nvim-notify", vim, "notify")
+    end,
+    opts = {
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 1000 })
+      end,
+    },
     config = function(_, opts)
       local notify = require "notify"
       notify.setup(opts)
@@ -159,7 +253,10 @@ return {
     end,
   },
   -- Telescope integration (:Telescope notify)
-  { "nvim-telescope/telescope.nvim", opts = function() require("telescope").load_extension "notify" end },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function() require("telescope").load_extension "notify" end,
+  },
 
   --  Code identation [guides]
   --  https://github.com/lukas-reineke/indent-blankline.nvim
@@ -228,7 +325,13 @@ return {
           disable_winbar_cb = function(args)
             return status.condition.buffer_matches({
               buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
-              filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+              filetype = {
+                "NvimTree",
+                "neo%-tree",
+                "dashboard",
+                "Outline",
+                "aerial",
+              },
             }, args.buf)
           end,
         },
@@ -237,7 +340,11 @@ return {
           hl = { fg = "fg", bg = "bg" },
           status.component.mode(),
           status.component.git_branch(),
-          status.component.file_info { filetype = {}, filename = false, file_modified = false },
+          status.component.file_info {
+            filetype = {},
+            filename = false,
+            file_modified = false,
+          },
           status.component.git_diff(),
           status.component.diagnostics(),
           status.component.fill(),
@@ -261,7 +368,10 @@ return {
             -- But you won't know the buffer name then while unfocused.
             -- This is specially important as we currently use a single status bar.
             status.component.file_info {
-              file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+              file_icon = {
+                hl = status.hl.file_icon "winbar",
+                padding = { left = 0 },
+              },
               file_modified = false,
               file_read_only = false,
               hl = status.hl.get_attributes("winbarnc", true),
@@ -269,7 +379,9 @@ return {
               update = "BufEnter",
             },
           },
-          status.component.breadcrumbs { hl = status.hl.get_attributes("winbar", true) },
+          status.component.breadcrumbs {
+            hl = status.hl.get_attributes("winbar", true),
+          },
         },
         tabline = { -- bufferline
           {
@@ -281,7 +393,12 @@ return {
                 vim.api.nvim_win_get_buf(self.winid)
               )
             end,
-            provider = function(self) return string.rep(" ", vim.api.nvim_win_get_width(self.winid) + 1) end,
+            provider = function(self)
+              return string.rep(
+                " ",
+                vim.api.nvim_win_get_width(self.winid) + 1
+              )
+            end,
             hl = { bg = "tabline_bg" },
           },
           status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
@@ -291,11 +408,19 @@ return {
             condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
             status.heirline.make_tablist { -- component for each tab
               provider = status.provider.tabnr(),
-              hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
+              hl = function(self)
+                return status.hl.get_attributes(
+                  status.heirline.tab_type(self, "tab"),
+                  true
+                )
+              end,
             },
             {
               -- close button for current tab
-              provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
+              provider = status.provider.close_button {
+                kind = "TabClose",
+                padding = { left = 1, right = 1 },
+              },
               hl = status.hl.get_attributes("tab_close", true),
               on_click = {
                 callback = function() require("base.utils.buffer").close_tab() end,
@@ -320,23 +445,37 @@ return {
 
       local function setup_colors()
         local Normal = get_hlgroup("Normal", { fg = C.fg, bg = C.bg })
-        local Comment = get_hlgroup("Comment", { fg = C.bright_grey, bg = C.bg })
+        local Comment =
+          get_hlgroup("Comment", { fg = C.bright_grey, bg = C.bg })
         local Error = get_hlgroup("Error", { fg = C.red, bg = C.bg })
-        local StatusLine = get_hlgroup("StatusLine", { fg = C.fg, bg = C.dark_bg })
+        local StatusLine =
+          get_hlgroup("StatusLine", { fg = C.fg, bg = C.dark_bg })
         local TabLine = get_hlgroup("TabLine", { fg = C.grey, bg = C.none })
-        local TabLineSel = get_hlgroup("TabLineSel", { fg = C.fg, bg = C.none })
+        local TabLineSel =
+          get_hlgroup("TabLineSel", { fg = C.fg, bg = C.none })
         local WinBar = get_hlgroup("WinBar", { fg = C.bright_grey, bg = C.bg })
         local WinBarNC = get_hlgroup("WinBarNC", { fg = C.grey, bg = C.bg })
-        local Conditional = get_hlgroup("Conditional", { fg = C.bright_purple, bg = C.dark_bg })
+        local Conditional =
+          get_hlgroup("Conditional", { fg = C.bright_purple, bg = C.dark_bg })
         local String = get_hlgroup("String", { fg = C.green, bg = C.dark_bg })
-        local TypeDef = get_hlgroup("TypeDef", { fg = C.yellow, bg = C.dark_bg })
-        local GitSignsAdd = get_hlgroup("GitSignsAdd", { fg = C.green, bg = C.dark_bg })
-        local GitSignsChange = get_hlgroup("GitSignsChange", { fg = C.orange, bg = C.dark_bg })
-        local GitSignsDelete = get_hlgroup("GitSignsDelete", { fg = C.bright_red, bg = C.dark_bg })
-        local DiagnosticError = get_hlgroup("DiagnosticError", { fg = C.bright_red, bg = C.dark_bg })
-        local DiagnosticWarn = get_hlgroup("DiagnosticWarn", { fg = C.orange, bg = C.dark_bg })
-        local DiagnosticInfo = get_hlgroup("DiagnosticInfo", { fg = C.white, bg = C.dark_bg })
-        local DiagnosticHint = get_hlgroup("DiagnosticHint", { fg = C.bright_yellow, bg = C.dark_bg })
+        local TypeDef =
+          get_hlgroup("TypeDef", { fg = C.yellow, bg = C.dark_bg })
+        local GitSignsAdd =
+          get_hlgroup("GitSignsAdd", { fg = C.green, bg = C.dark_bg })
+        local GitSignsChange =
+          get_hlgroup("GitSignsChange", { fg = C.orange, bg = C.dark_bg })
+        local GitSignsDelete =
+          get_hlgroup("GitSignsDelete", { fg = C.bright_red, bg = C.dark_bg })
+        local DiagnosticError =
+          get_hlgroup("DiagnosticError", { fg = C.bright_red, bg = C.dark_bg })
+        local DiagnosticWarn =
+          get_hlgroup("DiagnosticWarn", { fg = C.orange, bg = C.dark_bg })
+        local DiagnosticInfo =
+          get_hlgroup("DiagnosticInfo", { fg = C.white, bg = C.dark_bg })
+        local DiagnosticHint = get_hlgroup(
+          "DiagnosticHint",
+          { fg = C.bright_yellow, bg = C.dark_bg }
+        )
         local HeirlineInactive = get_hlgroup("HeirlineInactive", { bg = nil }).bg
           or status.hl.lualine_mode("inactive", C.dark_grey)
         local HeirlineNormal = get_hlgroup("HeirlineNormal", { bg = nil }).bg
@@ -417,8 +556,12 @@ return {
           "treesitter",
           "nav",
         } do
-          if not colors[section .. "_bg"] then colors[section .. "_bg"] = colors["section_bg"] end
-          if not colors[section .. "_fg"] then colors[section .. "_fg"] = colors["section_fg"] end
+          if not colors[section .. "_bg"] then
+            colors[section .. "_bg"] = colors["section_bg"]
+          end
+          if not colors[section .. "_fg"] then
+            colors[section .. "_fg"] = colors["section_fg"]
+          end
         end
         return colors
       end
@@ -431,7 +574,9 @@ return {
         pattern = "ColorScheme", -- This is an autocmd event
         group = augroup,
         desc = "Refresh heirline colors",
-        callback = function() require("heirline.utils").on_colorscheme(setup_colors()) end,
+        callback = function()
+          require("heirline.utils").on_colorscheme(setup_colors())
+        end,
       })
     end,
   },
@@ -442,7 +587,11 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        enabled = vim.fn.executable "make" == 1,
+        build = "make",
+      },
     },
     cmd = "Telescope",
     opts = function()
@@ -485,7 +634,11 @@ return {
       telescope.setup(opts)
       local utils = require "base.utils"
       local conditional_func = utils.conditional_func
-      conditional_func(telescope.load_extension, utils.is_available "telescope-fzf-native.nvim", "fzf")
+      conditional_func(
+        telescope.load_extension,
+        utils.is_available "telescope-fzf-native.nvim",
+        "fzf"
+      )
     end,
   },
 
@@ -493,14 +646,23 @@ return {
   --  https://github.com/mrjones2014/smart-splits.nvim
   {
     "mrjones2014/smart-splits.nvim",
-    opts = { ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" }, ignored_buftypes = { "nofile" } },
+    opts = {
+      ignored_filetypes = { "nofile", "quickfix", "qf", "prompt" },
+      ignored_buftypes = { "nofile" },
+    },
   },
 
   --  [better ui elements]
   --  https://github.com/stevearc/dressing.nvim
   {
     "stevearc/dressing.nvim",
-    init = function() require("base.utils").load_plugin_with_func("dressing.nvim", vim.ui, { "input", "select" }) end,
+    init = function()
+      require("base.utils").load_plugin_with_func(
+        "dressing.nvim",
+        vim.ui,
+        { "input", "select" }
+      )
+    end,
     opts = {
       input = {
         default_prompt = "➤ ",
@@ -508,7 +670,9 @@ return {
       },
       select = {
         backend = { "telescope", "builtin" },
-        builtin = { win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" } },
+        builtin = {
+          win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" },
+        },
       },
     },
   },
@@ -618,6 +782,7 @@ return {
           },
         },
         cursor = {
+          enable = false, -- We don't want cursor ghosting
           timing = animate.gen_timing.linear { duration = 26, unit = "total" },
         },
       }
