3cf192cb6d744c275fda802ec22b7ffc269c2131
Zeioth <zeioth@hotmail.com>
2023-05-29 00:00:28 +0200

    Cleanup

--- lua/base/utils/lsp.lua
+++ lua/base/utils/lsp.lua
@@ -9,14 +9,12 @@
 -- @copyright 2022
 -- @license GNU General Public License v3.0
 
-
 --    Functions:
 --       -> LSP settings
 --       -> Formatting settings
 --       -> setup
 --       -> on_attach
 --       -> config
-
 
 local M = {}
 local tbl_contains = vim.tbl_contains
@@ -31,9 +29,6 @@ local setup_handlers = {
 }
 
 M.diagnostics = { [0] = {}, {}, {}, {} } -- For diagnostics toggle in ./ui.lua
-
-
-
 
 -- LSP settings
 M.setup_diagnostics = function(signs)
@@ -56,12 +51,17 @@ M.setup_diagnostics = function(signs)
   -- For diagnostics toggle in ./ui.lua
   M.diagnostics = {
     -- diagnostics off
-    [0] = utils.extend_tbl(
-      default_diagnostics,
-      { underline = false, virtual_text = false, signs = false, update_in_insert = false }
-    ),
+    [0] = utils.extend_tbl(default_diagnostics, {
+      underline = false,
+      virtual_text = false,
+      signs = false,
+      update_in_insert = false,
+    }),
     -- status only
-    utils.extend_tbl(default_diagnostics, { virtual_text = false, signs = false }),
+    utils.extend_tbl(
+      default_diagnostics,
+      { virtual_text = false, signs = false }
+    ),
     -- virtual text off, signs on
     utils.extend_tbl(default_diagnostics, { virtual_text = false }),
     -- all diagnostics on
@@ -70,9 +70,6 @@ M.setup_diagnostics = function(signs)
 
   vim.diagnostic.config(M.diagnostics[vim.g.diagnostics_mode])
 end
-
-
-
 
 -- Default: Formating settings
 -- Enabled by default if there is a formatter present
@@ -87,11 +84,11 @@ M.format_opts.filter = function(client)
   local filter = M.formatting.filter
   local disabled = M.formatting.disabled or {}
   -- check if client is fully disabled or filtered by function
-  return not (vim.tbl_contains(disabled, client.name) or (type(filter) == "function" and not filter(client)))
+  return not (
+    vim.tbl_contains(disabled, client.name)
+    or (type(filter) == "function" and not filter(client))
+  )
 end
-
-
-
 
 --- Helper function to set up a given server with the Neovim LSP client
 ---@param server string The name of the server to be setup
@@ -101,9 +98,6 @@ M.setup = function(server)
   local setup_handler = setup_handlers[server] or setup_handlers[1]
   if setup_handler then setup_handler(server, opts) end
 end
-
-
-
 
 --- Helper function to check if any active LSP clients given a filter provide a specific capability
 ---@param capability string The server capability to check for (example: "documentFormattingProvider")
@@ -116,12 +110,18 @@ end
 ---@return boolean # Whether or not any of the clients provide the capability
 function M.has_capability(capability, filter)
   local clients = vim.lsp.get_active_clients(filter)
-  return not tbl_isempty(vim.tbl_map(function(client) return client.server_capabilities[capability] end, clients))
+  return not tbl_isempty(
+    vim.tbl_map(
+      function(client) return client.server_capabilities[capability] end,
+      clients
+    )
+  )
 end
 
 local function add_buffer_autocmd(augroup, bufnr, autocmds)
   if not vim.tbl_islist(autocmds) then autocmds = { autocmds } end
-  local cmds_found, cmds = pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
+  local cmds_found, cmds =
+      pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
   if not cmds_found or vim.tbl_isempty(cmds) then
     vim.api.nvim_create_augroup(augroup, { clear = false })
     for _, autocmd in ipairs(autocmds) do
@@ -135,12 +135,12 @@ local function add_buffer_autocmd(augroup, bufnr, autocmds)
 end
 
 local function del_buffer_autocmd(augroup, bufnr)
-  local cmds_found, cmds = pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
-  if cmds_found then vim.tbl_map(function(cmd) vim.api.nvim_del_autocmd(cmd.id) end, cmds) end
+  local cmds_found, cmds =
+      pcall(vim.api.nvim_get_autocmds, { group = augroup, buffer = bufnr })
+  if cmds_found then
+    vim.tbl_map(function(cmd) vim.api.nvim_del_autocmd(cmd.id) end, cmds)
+  end
 end
-
-
-
 
 --- The `on_attach` function used by Nvim
 ---@param client table The LSP client details when attaching
@@ -171,11 +171,13 @@ M.on_attach = function(client, bufnr)
   }
 
   if is_available "mason-lspconfig.nvim" then
-    lsp_mappings.n["<leader>li"] = { "<cmd>LspInfo<cr>", desc = "LSP information" }
+    lsp_mappings.n["<leader>li"] =
+    { "<cmd>LspInfo<cr>", desc = "LSP information" }
   end
 
   if is_available "null-ls.nvim" then
-    lsp_mappings.n["<leader>lI"] = { "<cmd>NullLsInfo<cr>", desc = "Null-ls information" }
+    lsp_mappings.n["<leader>lI"] =
+    { "<cmd>NullLsInfo<cr>", desc = "Null-ls information" }
   end
 
   if capabilities.codeActionProvider then
@@ -223,7 +225,10 @@ M.on_attach = function(client, bufnr)
     }
   end
 
-  if capabilities.documentFormattingProvider and not tbl_contains(M.formatting.disabled, client.name) then
+  if
+      capabilities.documentFormattingProvider
+      and not tbl_contains(M.formatting.disabled, client.name)
+  then
     lsp_mappings.n["<leader>lf"] = {
       function() vim.lsp.buf.format(M.format_opts) end,
       desc = "Format buffer",
@@ -239,22 +244,43 @@ M.on_attach = function(client, bufnr)
     local autoformat = M.formatting.format_on_save
     local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
     if
-      autoformat.enabled
-      and (tbl_isempty(autoformat.allow_filetypes or {}) or tbl_contains(autoformat.allow_filetypes, filetype))
-      and (tbl_isempty(autoformat.ignore_filetypes or {}) or not tbl_contains(autoformat.ignore_filetypes, filetype))
+        autoformat.enabled
+        and (tbl_isempty(autoformat.allow_filetypes or {}) or tbl_contains(
+          autoformat.allow_filetypes,
+          filetype
+        ))
+        and (
+          tbl_isempty(autoformat.ignore_filetypes or {})
+          or not tbl_contains(autoformat.ignore_filetypes, filetype)
+        )
     then
       add_buffer_autocmd("lsp_auto_format", bufnr, {
         events = "BufWritePre",
         desc = "autoformat on save",
         callback = function()
-          if not M.has_capability("documentFormattingProvider", { bufnr = bufnr }) then
+          if
+              not M.has_capability(
+                "documentFormattingProvider",
+                { bufnr = bufnr }
+              )
+          then
             del_buffer_autocmd("lsp_auto_format", bufnr)
             return
           end
           local autoformat_enabled = vim.b.autoformat_enabled
-          if autoformat_enabled == nil then autoformat_enabled = vim.g.autoformat_enabled end
-          if autoformat_enabled and ((not autoformat.filter) or autoformat.filter(bufnr)) then
-            vim.lsp.buf.format(require("base.utils").extend_tbl(M.format_opts, { bufnr = bufnr }))
+          if autoformat_enabled == nil then
+            autoformat_enabled = vim.g.autoformat_enabled
+          end
+          if
+              autoformat_enabled
+              and ((not autoformat.filter) or autoformat.filter(bufnr))
+          then
+            vim.lsp.buf.format(
+              require("base.utils").extend_tbl(
+                M.format_opts,
+                { bufnr = bufnr }
+              )
+            )
           end
         end,
       })
@@ -275,7 +301,12 @@ M.on_attach = function(client, bufnr)
         events = { "CursorHold", "CursorHoldI" },
         desc = "highlight references when cursor holds",
         callback = function()
-          if not M.has_capability("documentHighlightProvider", { bufnr = bufnr }) then
+          if
+              not M.has_capability(
+                "documentHighlightProvider",
+                { bufnr = bufnr }
+              )
+          then
             del_buffer_autocmd("lsp_document_highlight", bufnr)
             return
           end
@@ -325,7 +356,11 @@ M.on_attach = function(client, bufnr)
   if capabilities.signatureHelpProvider then
     lsp_mappings.n["<leader>lh"] = {
       function() vim.lsp.buf.signature_help() end,
-      desc = "Signature help",
+      desc = "Code help",
+    }
+    lsp_mappings.n["gh"] = {
+      function() vim.lsp.buf.signature_help() end,
+      desc = "Code help",
     }
   end
 
@@ -337,7 +372,10 @@ M.on_attach = function(client, bufnr)
   end
 
   if capabilities.workspaceSymbolProvider then
-    lsp_mappings.n["<leader>lG"] = { function() vim.lsp.buf.workspace_symbol() end, desc = "Search workspace symbols" }
+    lsp_mappings.n["<leader>lG"] = {
+      function() vim.lsp.buf.workspace_symbol() end,
+      desc = "Search workspace symbols",
+    }
   end
 
   if capabilities.semanticTokensProvider and vim.lsp.semantic_tokens then
@@ -348,28 +386,45 @@ M.on_attach = function(client, bufnr)
   end
 
   if is_available "telescope.nvim" then -- setup telescope mappings if available
-    if lsp_mappings.n.gd then lsp_mappings.n.gd[1] = function() require("telescope.builtin").lsp_definitions() end end
-    if lsp_mappings.n.gI then
-      lsp_mappings.n.gI[1] = function() require("telescope.builtin").lsp_implementations() end
+    if lsp_mappings.n.gd then
+      lsp_mappings.n.gd[1] = function()
+        require("telescope.builtin").lsp_definitions()
+      end
     end
-    if lsp_mappings.n.gr then lsp_mappings.n.gr[1] = function() require("telescope.builtin").lsp_references() end end
+    if lsp_mappings.n.gI then
+      lsp_mappings.n.gI[1] = function()
+        require("telescope.builtin").lsp_implementations()
+      end
+    end
+    if lsp_mappings.n.gr then
+      lsp_mappings.n.gr[1] = function()
+        require("telescope.builtin").lsp_references()
+      end
+    end
     if lsp_mappings.n["<leader>lR"] then
-      lsp_mappings.n["<leader>lR"][1] = function() require("telescope.builtin").lsp_references() end
+      lsp_mappings.n["<leader>lR"][1] = function()
+        require("telescope.builtin").lsp_references()
+      end
     end
     if lsp_mappings.n.gT then
-      lsp_mappings.n.gT[1] = function() require("telescope.builtin").lsp_type_definitions() end
+      lsp_mappings.n.gT[1] = function()
+        require("telescope.builtin").lsp_type_definitions()
+      end
     end
     if lsp_mappings.n["<leader>lG"] then
       lsp_mappings.n["<leader>lG"][1] = function()
         vim.ui.input({ prompt = "Symbol Query: " }, function(query)
-          if query then require("telescope.builtin").lsp_workspace_symbols { query = query } end
+          if query then
+            require("telescope.builtin").lsp_workspace_symbols { query = query }
+          end
         end)
       end
     end
   end
 
   if not vim.tbl_isempty(lsp_mappings.v) then
-    lsp_mappings.v["<leader>l"] = { desc = (vim.g.icons_enabled and " " or "") .. "LSP" }
+    lsp_mappings.v["<leader>l"] =
+    { desc = (vim.g.icons_enabled and " " or "") .. "LSP" }
   end
   utils.set_mappings(lsp_mappings, { buffer = bufnr })
 
@@ -379,22 +434,25 @@ end
 
 --- The default Nvim LSP capabilities
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
+M.capabilities.textDocument.completion.completionItem.documentationFormat =
+{ "markdown", "plaintext" }
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities.textDocument.completion.completionItem.preselectSupport = true
-M.capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-M.capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+M.capabilities.textDocument.completion.completionItem.insertReplaceSupport =
+    true
+M.capabilities.textDocument.completion.completionItem.labelDetailsSupport =
+    true
 M.capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-M.capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-M.capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+M.capabilities.textDocument.completion.completionItem.commitCharactersSupport =
+    true
+M.capabilities.textDocument.completion.completionItem.tagSupport =
+{ valueSet = { 1 } }
 M.capabilities.textDocument.completion.completionItem.resolveSupport =
-  { properties = { "documentation", "detail", "additionalTextEdits" } }
-M.capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
+{ properties = { "documentation", "detail", "additionalTextEdits" } }
+M.capabilities.textDocument.foldingRange =
+{ dynamicRegistration = false, lineFoldingOnly = true }
 M.capabilities = M.capabilities
 M.flags = {}
-
-
-
 
 --- Get the server configuration for a given language server to be provided to the server's `setup()` call
 ---@param server_name string The name of the server
@@ -408,18 +466,28 @@ function M.config(server_name)
   if server_name == "jsonls" then -- by default add json schemas
     local schemastore_avail, schemastore = pcall(require, "schemastore")
     if schemastore_avail then
-      lsp_opts.settings = { json = { schemas = schemastore.json.schemas(), validate = { enable = true } } }
+      lsp_opts.settings = {
+        json = {
+          schemas = schemastore.json.schemas(),
+          validate = { enable = true },
+        },
+      }
     end
   end
   if server_name == "yamlls" then -- by default add yaml schemas
     local schemastore_avail, schemastore = pcall(require, "schemastore")
-    if schemastore_avail then lsp_opts.settings = { yaml = { schemas = schemastore.yaml.schemas() } } end
+    if schemastore_avail then
+      lsp_opts.settings = { yaml = { schemas = schemastore.yaml.schemas() } }
+    end
   end
   if server_name == "lua_ls" then -- by default initialize neodev and disable third party checking
     pcall(require, "neodev")
     lsp_opts.before_init = function(config)
       if vim.b.neodev_enabled then
-        table.insert(config.settings.Lua.workspace.library, vim.fn.stdpath("config") .. "/lua")
+        table.insert(
+          config.settings.Lua.workspace.library,
+          vim.fn.stdpath "config" .. "/lua"
+        )
       end
     end
     lsp_opts.settings = { Lua = { workspace = { checkThirdParty = false } } }
