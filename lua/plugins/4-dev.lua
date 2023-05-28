ea6600289e40378d65448e837fe64596bfe778f2
Zeioth <zeioth@hotmail.com>
2023-05-29 01:19:30 +0200

    Docs: nvin-coverage

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -251,6 +251,14 @@ return {
 
   --  Shows a float panel with the [code coverage]
   --  https://github.com/andythigpen/nvim-coverage
+  --
+  --  Your project must generate coverage/lcov.info for this to work.
+  --
+  --  On jest, make sure your packages.json file has this:
+  --  "test": "jest --coverage"
+  --
+  --  If you use other framework or language, refer to nvim-coverage docs:
+  --  https://github.com/andythigpen/nvim-coverage/blob/main/doc/nvim-coverage.txt
   {
     "andythigpen/nvim-coverage",
     cmd = {
@@ -263,6 +271,7 @@ return {
       "CoverageClear",
       "CoverageSummary",
     },
+    config = function() require("coverage").setup() end,
     requires = { "nvim-lua/plenary.nvim" },
   },
 
