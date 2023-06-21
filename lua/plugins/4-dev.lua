66bb25fb71ddd5faa63bc1d0b3e4fc1eeab28729
Zeioth <zeioth@hotmail.com>
2023-06-21 18:18:29 +0200

    feat(compiler.nvim) shipped by default for the first time ever. only for c, c++ and make for now.

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -16,6 +16,10 @@
 --       ## DEBUGGER
 --       -> nvim-dap                       [debugger]
 
+--       ## COMPILER
+--       -> compiler.nvim                  [compiler]
+--       -> overseer.nvim                  [task runner]
+
 --       ## TESTING
 --       -> neotest.nvim                   [unit testing]
 
@@ -204,6 +208,43 @@ return {
     },
     event = "User BaseFile",
   },
+
+  --  COMPILER ----------------------------------------------------------------
+  {
+    "https://github.com/Zeioth/compiler.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults"},
+    config = function(_, opts) require("compiler").setup(opts) end,
+  },
+  {
+    "stevearc/overseer.nvim",
+    cmd = {"CompilerOpen", "CompilerToggleResults"},
+    opts = {
+      -- Tasks are disposed 5 minutes after running to free resources.
+      -- If you need to close a task inmediatelly:
+      -- press ENTER in the outut menu on the task you wanna close.
+      task_list = { -- this refers to the window that shows the result
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+        bindings = {
+          ["q"] = function() vim.cmd("OverseerClose") end ,
+        }
+      },
+      -- component_aliases = { -- undomment this to disable notifications
+      --   -- Components included in default will apply to all tasks
+      --   default = {
+      --     { "display_duration", detail_level = 2 },
+      --     "on_output_summarize",
+      --     "on_exit_set_status",
+      --     "on_complete_notify",
+      --     "on_complete_dispose",
+      --   },
+      -- },
+    },
+    config = function(_, opts) require("overseer").setup(opts) end,
+  },
+
 
   --  TESTING ----------------------------------------------------------------
   --  Run tests inside of nvim [unit testing]
