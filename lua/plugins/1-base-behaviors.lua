fe2a49fa108d0cee351b33700a59cff309d3a86d
Zeioth <zeioth@hotmail.com>
2023-06-26 22:44:45 +0200

    cleanup(zenmode) remove unused stuff.

--- lua/plugins/1-base-behaviors.lua
+++ lua/plugins/1-base-behaviors.lua
@@ -546,11 +546,6 @@ return {
   {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
   },
 
   --  nvim-neoclip [nvim clipboard]
