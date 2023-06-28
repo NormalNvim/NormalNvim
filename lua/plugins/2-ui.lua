3769b5d808577174397bae41b97bc96716df775e
Zeioth <zeioth@hotmail.com>
2023-06-28 13:55:01 +0200

    fix(heirline) Super obscure border case stack trace errors fixed. The cause was we were instancing heirline before lsp could be ready. We already control this scenario with the vim.lsp.get_active_clients() condition, but in some instances (anidated nvim sessions) heirline seem to miss interpret this condition. By changing the event to VeryLazy, we give extra some time to LSP to be ready in those scenarios.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -241,7 +241,7 @@ return {
   --  https://github.com/rebelot/heirline.nvim
   {
     "rebelot/heirline.nvim",
-    event = "BufEnter",
+    event = "VeryLazy",
     opts = function()
       local status = require "base.utils.status"
       return {
