5b604fe95623335f0f9f8410f9f174a320f6f4c7
Zeioth <zeioth@hotmail.com>
2023-08-24 16:05:58 +0200

    feat(DAP) Added F# config (tested and working).

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -358,6 +358,9 @@ return {
         },
       }
 
+      -- F#
+      dap.configurations.fsharp = dap.configurations.cs
+
       -- Java
       -- Note: The java debugger jdtls is automatically spawned and configured
       --       when a java file is opened. You can check it here:
