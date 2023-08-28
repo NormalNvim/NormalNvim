373e0febb4c93d5c92e49e43683a1f802b67df0f
Zeioth <zeioth@hotmail.com>
2023-08-28 16:57:40 +0200

    fix(neotree): Ensure the neotree bufs list is updated no matter how you open a buffer. See https://github.com/nvim-neo-tree/neo-tree.nvim/issues/1124

--- lua/base/3-autocmds.lua
+++ lua/base/3-autocmds.lua
@@ -172,7 +172,7 @@ autocmd({ "BufWritePost" }, {
   end,
 })
 
--- 5. Update neotree when closin the git client.
+-- 5. Update neotree when closin the git client, or when you focus a buffer.
 if is_available "neo-tree.nvim" then
   autocmd("TermClose", {
     pattern = { "*lazygit", "*gitui" },
@@ -181,6 +181,15 @@ if is_available "neo-tree.nvim" then
     callback = function()
       if package.loaded["neo-tree.sources.git_status"] then
         require("neo-tree.sources.git_status").refresh()
+      end
+    end,
+  })
+  autocmd("BufReadPre", {
+    desc = "Refresh Neo-Tree buffers tab when opening a new buffer",
+    group = augroup("neotree_git_refresh", { clear = true }),
+    callback = function()
+      if package.loaded["neo-tree.sources.buffers"] then
+        require("neo-tree.sources.buffers").buffers_changed()
       end
     end,
   })
