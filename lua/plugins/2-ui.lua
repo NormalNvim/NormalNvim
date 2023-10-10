a84223be2d65b0083dedf4340d140a584d0ca43e
Zeioth <zeioth@hotmail.com>
2023-10-10 18:45:19 +0200

    fix simplified

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -249,7 +249,7 @@ return {
       -- Disable for certain filetypes
       vim.api.nvim_create_autocmd({ "FileType" }, {
         desc = "Disable indentscope for certain filetypes",
-        callback = function(event)
+        callback = function()
           local ignore_filetypes = {
             "alpha", -- also see alpha keymapping
             "aerial",
@@ -267,7 +267,7 @@ return {
             "Trouble"
           }
           if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b[event.buf].miniindentscope_disable = true
+            vim.b.miniindentscope_disable = true
           end
         end,
       })
