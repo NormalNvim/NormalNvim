e93d186577b092460ad663859be56da3073c2088
Zeioth <zeioth@hotmail.com>
2023-10-16 02:41:14 +0200

    fix: Latest overseer version adds the keymapping we already have, so let's make sure it doesn't trigger twice.

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -313,9 +313,6 @@ return {
         min_height = 25,
         max_height = 25,
         default_detail = 1,
-        bindings = {
-          ["q"] = function() vim.cmd("OverseerClose") end ,
-        }
       },
       -- component_aliases = { -- uncomment this to disable notifications
       --   -- Components included in default will apply to all tasks
