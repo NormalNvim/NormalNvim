d60999472db203436647b07a162dae37534511a7
Zeioth <zeioth@hotmail.com>
2023-12-19 16:43:27 +0100

    ✨feat(nvim-java) New plugin that covers the border case of providing jdtls support.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -9,6 +9,7 @@
 --       -> nvim-colorizer                 [hex colors]
 
 --       ## LSP
+--       -> nvim-java                      [java support]
 --       -> nvim-lspconfig                 [lsp config]
 --       -> garbage-day                    [lsp garbage collector]
 --       -> mason.nvim                     [lsp package manager]
@@ -147,6 +148,33 @@ return {
   },
 
   --  LSP -------------------------------------------------------------------
+
+  -- nvim-java [java support]
+  -- https://github.com/neovim/nvim-lspconfig
+  -- Reliable jdtls support. Must go before lsp-config.
+  {
+    'nvim-java/nvim-java',
+    ft = { "java" },
+    dependencies = {
+      'nvim-java/lua-async-await',
+      'nvim-java/nvim-java-core',
+      'nvim-java/nvim-java-test',
+      'nvim-java/nvim-java-dap',
+      'MunifTanjim/nui.nvim',
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        },
+      }
+    },
+  },
+
   --  Syntax highlight [lsp config]
   --  https://github.com/neovim/nvim-lspconfig
   {
