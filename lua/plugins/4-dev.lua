856816a08b2f72737a9caa0e62e1f6a6d23ce4b9
Zeioth <zeioth@hotmail.com>
2024-03-11 18:42:23 +0100

    fix(`compiler.nvim`): The plugin is now loaded if you try to run `:CompilerStop`, instead of throwing a error.

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -301,7 +301,9 @@ return {
   --  https://github.com/Zeioth/compiler.nvim
   {
     "Zeioth/compiler.nvim",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    cmd = {
+      "CompilerOpen", "CompilerToggleResults", "CompilerRedo", "CompilerStop"
+    },
     dependencies = { "stevearc/overseer.nvim" },
     opts = {},
   },
