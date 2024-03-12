08b028580eae6b6296b274fd6d816bc6961ad690
Zeioth <zeioth@hotmail.com>
2024-03-12 22:12:50 +0100

    change(aerial): Structs are data structures, and we only want functions, so let's disable that.

--- lua/plugins/4-dev.lua
+++ lua/plugins/4-dev.lua
@@ -185,7 +185,7 @@ return {
         "Interface",
         -- "Module",
         "Method",
-        "Struct",
+        -- "Struct",
       },
       open_automatic = false, -- Open if the buffer is compatible
       autojump = true,
