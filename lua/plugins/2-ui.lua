bdf91186a0fd7a9f2fc6831aa0002878a120940c
Zeioth <zeioth@hotmail.com>
2024-03-23 04:32:35 +0100

    ✨feat(`notify`): Now 144fps smooth on PC. 30 fps on Android to save energy.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -209,25 +209,33 @@ return {
   {
     "rcarriga/nvim-notify",
     event = "User BaseDefered",
-    opts = {
-      max_height = function() return math.floor(vim.o.lines * 0.75) end,
-      max_width = function() return math.floor(vim.o.columns * 0.75) end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 175 })
-        if not vim.g.notifications_enabled then
-          vim.api.nvim_win_close(win, true)
-        end
-        if not package.loaded["nvim-treesitter"] then
-          pcall(require, "nvim-treesitter")
-        end
-        vim.wo[win].conceallevel = 3
-        local buf = vim.api.nvim_win_get_buf(win)
-        if not pcall(vim.treesitter.start, buf, "markdown") then
-          vim.bo[buf].syntax = "markdown"
-        end
-        vim.wo[win].spell = false
-      end,
-    },
+    opts = function()
+      local fps
+      if not is_android then fps = 144 else fps = 30 end
+
+      return {
+        timeout = 2500,
+        fps = fps,
+        max_height = function() return math.floor(vim.o.lines * 0.75) end,
+        max_width = function() return math.floor(vim.o.columns * 0.75) end,
+        on_open = function(win)
+          -- enable markdown support on notifications
+          vim.api.nvim_win_set_config(win, { zindex = 175 })
+          if not vim.g.notifications_enabled then
+            vim.api.nvim_win_close(win, true)
+          end
+          if not package.loaded["nvim-treesitter"] then
+            pcall(require, "nvim-treesitter")
+          end
+          vim.wo[win].conceallevel = 3
+          local buf = vim.api.nvim_win_get_buf(win)
+          if not pcall(vim.treesitter.start, buf, "markdown") then
+            vim.bo[buf].syntax = "markdown"
+          end
+          vim.wo[win].spell = false
+        end,
+      }
+    end,
     config = function(_, opts)
       local notify = require "notify"
       notify.setup(opts)
