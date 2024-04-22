f5f014e493755f605efa15c2b9cf6443b285bbc7
Zeioth <zeioth@hotmail.com>
2024-04-22 18:03:03 +0200

    comments(trim.nvim): Now suggest a config to ignore note taking buffers, which is a quite common case of use.

--- lua/plugins/1-base-behaviors.lua
+++ lua/plugins/1-base-behaviors.lua
@@ -102,11 +102,11 @@ return {
     "cappyzawa/trim.nvim",
     event = "BufWrite",
     opts = {
-      -- ft_blocklist = {"typescript"},
       trim_on_write = true,
       trim_trailing = true,
       trim_last_line = false,
       trim_first_line = false,
+      -- ft_blocklist = { "markdown", "text", "org", "tex", "asciidoc", "rst" },
       -- patterns = {[[%s/\(\n\n\)\n\+/\1/]]}, -- Only one consecutive bl
     },
   },
