03db7c1e518033288ed445c0dac7a51e7057d191
Zeioth <zeioth@hotmail.com>
2024-05-22 01:01:42 +0200

    ✨feat(none-ls-autoload): Added support for legacy none-ls sources.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -263,15 +263,37 @@ return {
     event = "User BaseFile",
     dependencies = {
       "williamboman/mason.nvim",
-      "nvimtools/none-ls-extras.nvim" -- To install external sources from a repo.
+      "zeioth/none-ls-external-sources.nvim"
     },
     opts = {
-      external_sources = { -- To indicate where to find a external source.
-        'none-ls.formatting.reformat_gherkin'
+      -- Here you can add support for sources not oficially suppored by none-ls.
+      external_sources = {
+        -- diagnostics
+        'none-ls-external-sources.diagnostics.cpplint',
+        'none-ls-external-sources.diagnostics.eslint',
+        'none-ls-external-sources.diagnostics.eslint_d',
+        'none-ls-external-sources.diagnostics.flake8',
+        'none-ls-external-sources.diagnostics.luacheck',
+        'none-ls-external-sources.diagnostics.php',
+        'none-ls-external-sources.diagnostics.psalm',
+        'none-ls-external-sources.diagnostics.shellcheck',
+        'none-ls-external-sources.diagnostics.yamllint',
+
+        -- formatting
+        'none-ls-external-sources.formatting.autopep8',
+        'none-ls-external-sources.formatting.beautysh',
+        'none-ls-external-sources.formatting.easy-coding-standard',
+        'none-ls-external-sources.formatting.eslint',
+        'none-ls-external-sources.formatting.eslint_d',
+        'none-ls-external-sources.formatting.jq',
+        'none-ls-external-sources.formatting.latexindent',
+        'none-ls-external-sources.formatting.reformat_gherkin',
+        'none-ls-external-sources.formatting.rustfmt',
+        'none-ls-external-sources.formatting.standardrb',
+        'none-ls-external-sources.formatting.yq',
       },
     },
   },
-
 
   --  Schema Store [mason extra schemas]
   --  https://github.com/b0o/SchemaStore.nvim
