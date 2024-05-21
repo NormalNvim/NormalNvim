74a3a0f89fdb48f4616595774f0433871ffb3f1c
Zeioth <zeioth@hotmail.com>
2024-05-21 23:22:27 +0200

    refactor: decapitalize 'Zeioth' so all plugins authored by me appear together in `lazy_snapshot.lua`.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -225,10 +225,10 @@ return {
 
   --  mason [lsp package manager]
   --  https://github.com/williamboman/mason.nvim
-  --  https://github.com/Zeioth/mason-extra-cmds
+  --  https://github.com/zeioth/mason-extra-cmds
   {
     "williamboman/mason.nvim",
-    dependencies = { "Zeioth/mason-extra-cmds", opts = {} },
+    dependencies = { "zeioth/mason-extra-cmds", opts = {} },
     cmd = {
       "Mason",
       "MasonInstall",
