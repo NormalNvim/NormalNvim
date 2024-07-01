01558e7f9a75f86a29e4fc2a502566666c96fa27
Zeioth <zeioth@hotmail.com>
2024-07-01 11:30:31 +0200

    fix(telescope): remove mappings that break default telescope behavior ← From upstream.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -410,8 +410,6 @@ return {
       local actions = require("telescope.actions")
       local mappings = {
         i = {
-          ["<C-n>"] = actions.cycle_history_next,
-          ["<C-p>"] = actions.cycle_history_prev,
           ["<C-j>"] = actions.move_selection_next,
           ["<C-k>"] = actions.move_selection_previous,
           ["<ESC>"] = actions.close,
