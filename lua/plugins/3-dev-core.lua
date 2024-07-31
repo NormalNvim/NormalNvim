ee8c70c76e7f3e2e11dbfe8a5e95e8ce29f416e1
Zeioth <zeioth@hotmail.com>
2024-07-31 03:03:14 +0200

    revert: this was accidentally changed by search and replace.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -510,7 +510,7 @@ return {
       }
 
       -- helper
-      local function has_mods_before()
+      local function has_words_before()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
       end
@@ -608,7 +608,7 @@ return {
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_mods_before() then
+            elseif has_words_before() then
               cmp.complete()
             else
               fallback()
