4e4a7e3c2dbe913328552ee48a857950bef2933a
Zeioth <zeioth@hotmail.com>
2024-10-04 23:23:17 +0200

    ⚡perf(`init.lua`): New strategy to load stuff async.

--- init.lua
+++ init.lua
@@ -3,27 +3,46 @@
 -- This is the entry point of your config.
 -- ---------------------------------------
 
--- LOAD SOURCE FILES BY ORDER:
-vim.loader.enable()
-for _, source in ipairs {
-  "base.1-options",
-  "base.2-lazy",
-  "base.3-autocmds",
-  "base.4-mappings",
-} do
+local function load_source(source)
   local status_ok, error = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. error) end
+  if not status_ok then
+    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. error)
+  end
 end
 
--- ONCE ALL SOURCE FILES HAVE LOADED:
--- Load the color scheme defined in ./lua/1-options.lua
-vim.defer_fn(function() -- in a non blocking way.
-  if vim.g.default_colorscheme then
-    if not pcall(vim.cmd.colorscheme, vim.g.default_colorscheme) then
-      require("base.utils").notify(
-        "Error setting up colorscheme: " .. vim.g.default_colorscheme,
-        vim.log.levels.ERROR
-      )
-    end
+local function load_sources(source_files)
+  vim.loader.enable()
+  for _, source in ipairs(source_files) do
+    load_source(source)
   end
-end, 0)
+end
+
+local function load_sources_async(source_files)
+  for _, source in ipairs(source_files) do
+    vim.defer_fn(function()
+      load_source(source)
+    end, 50)
+  end
+end
+
+local function load_colorscheme_async(colorscheme)
+  vim.defer_fn(function()
+    if vim.g.default_colorscheme then
+      if not pcall(vim.cmd.colorscheme, colorscheme) then
+        require("base.utils").notify(
+          "Error setting up colorscheme: " .. colorscheme,
+          vim.log.levels.ERROR
+        )
+      end
+    end
+  end, 0)
+end
+
+-- Call the functions defined above.
+load_sources({
+  "base.1-options",
+  "base.2-lazy",
+  "base.3-autocmds", -- critical stuff, don't change the execution order.
+})
+load_colorscheme_async(vim.g.default_colorscheme)
+load_sources_async({ "base.4-mappings" })
