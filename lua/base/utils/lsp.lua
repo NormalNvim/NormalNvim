2d2697fb1ba240efb936e73e1916f3c6e27ea9b1
Zeioth <zeioth@hotmail.com>
2024-10-17 00:25:55 +0200

    comments

--- lua/base/utils/lsp.lua
+++ lua/base/utils/lsp.lua
@@ -153,7 +153,7 @@ function M.apply_user_lsp_settings(server_name)
     local is_schemastore_loaded, schemastore = pcall(require, "schemastore")
     if is_schemastore_loaded then opts.settings = { yaml = { schemas = schemastore.yaml.schemas() } } end
   end
-  if server_name == "bashls" then -- Use mason's shellcheck instead of OS
+  if server_name == "bashls" then -- Set shellcheck path to mason
     opts.settings = { bashIde = { shellcheckPath = vim.fn.stdpath "data" .. "/mason/bin/shellcheck" } }
   end
 
