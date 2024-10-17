99df7d20beea853c080d65cd6409b06862db8286
Zeioth <zeioth@hotmail.com>
2024-10-17 18:35:54 +0200

    refactor(`nvim-web-devicons`): These icons have been included into the plugin, so no need for use to define them manually anymore.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -567,20 +567,8 @@ return {
       override = {
         default_icon = {
           icon = require("base.utils").get_icon("DefaultFile"),
-          name = "default"
+          name = "default",
         },
-        deb = { icon = "", name = "Deb" },
-        lock = { icon = "󰌾", name = "Lock" },
-        mp3 = { icon = "󰎆", name = "Mp3" },
-        mp4 = { icon = "", name = "Mp4" },
-        out = { icon = "", name = "Out" },
-        ["robots.txt"] = { icon = "󰚩", name = "Robots" },
-        ttf = { icon = "", name = "TrueTypeFont" },
-        rpm = { icon = "", name = "Rpm" },
-        woff = { icon = "", name = "WebOpenFontFormat" },
-        woff2 = { icon = "", name = "WebOpenFontFormat2" },
-        xz = { icon = "", name = "Xz" },
-        zip = { icon = "", name = "Zip" },
       },
     },
     config = function(_, opts)
@@ -637,7 +625,7 @@ return {
         "noice",
         "prompt",
         "TelescopePrompt",
-        "alpha",
+        "alpha"
       },
     },
   },
