a05a2fdc4f7cb524abc75395c2bf4aa51cf425b9
Zeioth <zeioth@hotmail.com>
2025-07-20 02:15:21 +0200

    change(nvim-notify): now defaults from 144fps to 160fps for animations, on pc.

--- lua/plugins/2-ui.lua
+++ lua/plugins/2-ui.lua
@@ -221,7 +221,7 @@ return {
     event = "User BaseDefered",
     opts = function()
       local fps
-      if is_android then fps = 30 else fps = 144 end
+      if is_android then fps = 30 else fps = 160 end
 
       return {
         timeout = 2500,
