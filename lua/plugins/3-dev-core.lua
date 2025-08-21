73311cf1e2092c8f946f85a03f3fcd02406e929b
Zeioth <zeioth@hotmail.com>
2025-08-21 20:16:20 +0200

    refactor: This line is not really necessary.

--- lua/plugins/3-dev-core.lua
+++ lua/plugins/3-dev-core.lua
@@ -233,7 +233,6 @@ return {
 
       -- Apply lsp mappings to lsp clients.
       vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           local bufnr = args.buf
